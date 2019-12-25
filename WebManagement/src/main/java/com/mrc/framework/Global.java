296aaa1b41036195bb05cf094f6b90165c45cd99
전상훈 <mrc0700@hotmail.com>
2019-12-25 16:06:29 +0900

    공통추가

---
+++ WebManagement/src/main/java/com/mrc/framework/Global.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.mrc.framework;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+
+
+/**
+ * @author jsh
+ *
+ */
+@Component
+public class Global {
+
+	public static CommonInfo CommonInfo;
+	public static SecurityInfo SecurityInfo;
+	
+	@PostConstruct
+	private void initialize() {
+		CommonInfo = new CommonInfo();
+		SecurityInfo = new SecurityInfo();
+	}
+}
---
+++ WebManagement/src/main/java/com/mrc/framework/SecurityInfo.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.mrc.framework;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author jsh
+ *
+ */
+public class SecurityInfo {
+	public  String encryptSHA256(String str)
+	{
+		 String SHA = null;
+		    try {
+		        MessageDigest sh = MessageDigest.getInstance("SHA-256"); // 이 부분을 SHA-1으로 바꿔도 된다!
+		        sh.update(str.getBytes()); 
+		        byte byteData[] = sh.digest();
+		        StringBuffer sb = new StringBuffer(); 
+		        for(int i = 0 ; i < byteData.length ; i++){
+		            sb.append(Integer.toString((byteData[i]&0xff) + 0x100, 16).substring(1));
+		        }
+		        SHA = sb.toString();
+		    } catch(NoSuchAlgorithmException e){
+		        e.printStackTrace(); 
+		    }
+		    return SHA;
+	}
+}
