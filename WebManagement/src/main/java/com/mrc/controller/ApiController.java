8d9399f91d590cca8d0a5222f52d25709fb99dc3
전상훈 <mrc0700@hotmail.com>
2019-12-30 08:50:24 +0900

    수정사항

--- WebManagement/src/main/java/com/mrc/controller/ApiController.java
+++ WebManagement/src/main/java/com/mrc/controller/ApiController.java
@@ -5,6 +5,7 @@ package com.mrc.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mrc.db.dao.ApiDao;
-import com.mrc.db.dto.T_MEMBER_EMPLOYEE;
+import com.mrc.db.dao.MemberDao;
 import com.mrc.db.dto.member_cond;
 import com.mrc.db.dto.t_member;
-import com.mrc.db.mapper.MemberMapper;
 import com.mrc.framework.Global;
 
 /**
@@ -28,7 +28,7 @@ public class ApiController {
 	private ApiDao apiDao;
 
 	@Autowired
-	private MemberMapper mapper;
+	private MemberDao memberDao;
 
 	@GetMapping(path = "/")
 	public String index() {
@@ -45,37 +45,33 @@ public class ApiController {
 	@GetMapping(path = "/memberlist")
 	public List<t_member> memberlist() {
 
-		T_MEMBER_EMPLOYEE abc = new T_MEMBER_EMPLOYEE();
+		
 
-		List<t_member> list = mapper.getMemberList(member_cond.builder()
+		List<t_member> list = memberDao.memberlist(member_cond.builder()
 				.member_code(1)
 				.member_id("mrc0700@gmail.com")
-				.member_pw(Global.SecurityInfo.encryptSHA256("1111")).build());
+				.member_pw("1111").build());
+		
+		
 		// return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
 		return list;
 	}
 
 	@GetMapping(path = "/saveMember")
 	public String saveMember() {
-		String rtn = "";
-		try {
+		/*임의 param start*/
+		List<t_member> list = new ArrayList();
+		
 			for (int i = 1; i <= 10; i++) {
-				t_member member = t_member.builder()
+				list.add(t_member.builder()
 				.member_code(0)
 				.member_id("a" + Global.DataInfo.LPad(Integer.toString(i), 2, "0") + "@gmail.com")
 				.member_name("회원" + Global.DataInfo.LPad("a", 2, "0"))
 				.member_pw(Global.SecurityInfo.encryptSHA256("1111"))
-				.update_code(0).build();
-				
-				mapper.memberSave(member);
-				
+				.update_code(0).build());
 			}
-		} catch (Exception ex) {
-			rtn = ex.getMessage();
-		}
-
-		if (rtn.equals(""))
-			rtn = "저장에 성공하였습니다.";
+		/*임의 param end*/
+		String rtn = memberDao.memberSave(list);
 		return rtn;
 	}
 }
--- WebManagement/src/main/java/com/mrc/db/dao/MemberDao.java
+++ WebManagement/src/main/java/com/mrc/db/dao/MemberDao.java
@@ -5,12 +5,13 @@ package com.mrc.db.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.mrc.db.dto.member_cond;
 import com.mrc.db.dto.t_member;
+import com.mrc.db.mapper.MemberMapper;
+import com.mrc.framework.Global;
 
 /**
  * @author jsh
@@ -18,14 +19,29 @@ import com.mrc.db.dto.t_member;
  */
 @Repository
 public class MemberDao {
-	protected static final String NAMESPACE = "com.mrc.db.dao.";
-
 	@Autowired
-	private SqlSession sqlSession;
-	
-	public List<t_member> getMemberList(member_cond Cond){
-		//return "";
-		
-		return 	sqlSession.selectList(NAMESPACE + "getMemberList", Cond);
+	private MemberMapper mapper;
+
+	public List<t_member> memberlist(member_cond member) {
+
+		member.setMember_pw(Global.SecurityInfo.encryptSHA256(member.getMember_pw()));
+		List<t_member> list = mapper.getMemberList(member);
+		// return LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+		return list;
 	}
+
+	public String memberSave(List<t_member> list) {
+		String rtn = "";
+		try {
+			for (int i = 0; i < list.size(); i++) {
+				mapper.memberSave(list.get(i));
+			}
+		} catch (Exception ex) {
+			rtn = ex.getMessage();
+		}
+		if (rtn.equals(""))
+			rtn = "저장에 성공하였습니다.";
+		return rtn;
+	}
+
 }
