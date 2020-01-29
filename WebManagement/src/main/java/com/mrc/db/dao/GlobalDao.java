3341438bbbd9597fb7f69161faeec6fbaa97931a
전상훈 <mrc0700@hotmail.com>
2020-01-30 00:08:48 +0900

    GlobalDao 추가

---
+++ WebManagement/src/main/java/com/mrc/db/dao/GlobalDao.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.mrc.db.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mrc.db.dto.member_cond;
+import com.mrc.db.dto.t_member;
+import com.mrc.db.dto.common.SelectItem;
+import com.mrc.db.dto.common.common_cond;
+import com.mrc.db.dto.common.t_common;
+
+/**
+ * @author jsh
+ *
+ */
+@Component
+public class GlobalDao {
+	
+	public static IMemberDao<member_cond, t_member> MemberDao;
+	public static ICommonDao<common_cond, t_common, SelectItem> CommonDao;
+	public static ApiDao ApiDao;
+	
+	@Autowired
+	private GlobalDao(IMemberDao<member_cond, t_member> member, ICommonDao<common_cond, t_common, SelectItem> common,  ApiDao api) {
+		MemberDao = member;
+		CommonDao = common;
+		ApiDao = api;
+	}
+	
+}
