ce39e098e7a51fbdcfc72f20f4ba07ffd423dbe1
전상훈 <mrc0700@hotmail.com>
2020-01-31 09:31:44 +0900

    Dao Interface 팩키지 폴더로이동

---
+++ WebManagement/src/main/java/com/mrc/db/mapper/MemberMapper.java
@@ -0,0 +1,34 @@
+package com.mrc.db.mapper;
+
+import java.util.List;
+
+import com.mrc.db.dto.member_cond;
+import com.mrc.db.dto.t_member;
+
+public class MemberMapper implements IMemberMapper {
+
+	@Override
+	public void Save(t_member data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<t_member> getList(member_cond Cond) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(t_member data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(t_member data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
